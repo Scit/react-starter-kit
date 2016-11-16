9836d4d36ad7201ebb53d85c3909fbc443afcecb
Andrey Terenkov <andrey.terenkov@gmail.com>
2016-11-16 23:52:42 +0300

    First commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Frame from './components/Frame'
+
+export default class App extends React.Component {
+  render() {
+    return (
+    	<Frame>
+				<h1>Hello</h1>
+      </Frame>
+    )
+  }
+}
